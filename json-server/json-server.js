908697736ad77ae6bc908713ed2f79c3f71cab28
Camdyn Rasque <rasque.camdyn@gmail.com>
2021-12-21 17:22:01 -0800

    Updated json-server to fix routing issue

--- json-server/json-server.js
+++ json-server/json-server.js
@@ -5,8 +5,18 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+// User the middlewares to help with processes
 server.use(middlewares);
+
+// Rewrite the routes to remove the /json-server/
+server.use(jsonServer.rewriter({
+  '/json-server/*': '/$1'
+}));
+
+// Use the router with the new routes
 server.use(router);
+
+// Run the server
 server.listen(3004, () => {
   console.log('JSON Server is running');
 });
